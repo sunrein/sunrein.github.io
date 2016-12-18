92664627f45d84383c12e2ab8947b5b1d3e838af
sunrein <sabrina.unrein@gmail.com>
2016-12-17 20:36:12 -0700

    get jquery working! :D

--- public/js/application.js
+++ public/js/application.js
@@ -1,1 +1,17 @@
-$(document).ready();
+// example taken from CodeAcademy exercise. Makes button fade in and out when selected
+// by the cursor, may be fun to experiment with on site
+
+$(document).ready(function() {
+    $('a').mouseover(function() {
+        $('a').fadeTo('fast', 0.6);
+    });
+
+    $('a').mouseleave(function() {
+        $('a').fadeTo('fast', 1);
+    });
+});
+
+// jquery effects: https://api.jquery.com/category/effects/
+
+// jquery projects to try:
+// birdman
